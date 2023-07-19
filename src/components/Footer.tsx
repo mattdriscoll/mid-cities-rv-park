588e2b713b06b769529abce8d66ab02f8f16434d
Matt Driscoll <matt@815dev.com>
2023-07-18 21:57:47 -0500

    Tweak footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <footer className="mt-auto bg-mcrv-black text-mcrv-white py-2">
       <SectionContainer>
         <div className="flex flex-row justify-between items-center">
-          <p className="text-center text-sm">
+          <p className="text-sm mb-0">
             &copy; {new Date().getFullYear()} Mid Cities RV Park
           </p>
-          <p>
+          <p className="text-sm mb-0">
             Built by{" "}
             <Link href="https://815dev.io" className="">
               815dev
